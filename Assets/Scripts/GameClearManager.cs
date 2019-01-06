3d581b08a73e96eb336ec7bf4d4c13919d18edf8
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2019-01-07 01:30:57 +0900

    ▶︎GameOverの完成 ▶︎GameClearの完成

---
+++ Assets/Scripts/GameClearManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//シーン遷移扱う
+
+public class GameClearManager : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	//Titleボタン押した時に呼ばれる関数
+	public void TitleButton()
+	{
+		SceneManager.LoadScene("Title");
+	}
+}
