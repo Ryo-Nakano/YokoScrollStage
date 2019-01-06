8c9a8539f876f5cdfe629ae9fb6b1ddf6e978beb
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2019-01-06 21:15:46 +0900

    ▶︎Titleシーン完成

---
+++ Assets/Scripts/StartSceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//シーン遷移扱う
+
+public class StartSceneManager : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+    //Startボタンを押した時に呼ばれる関数
+	public void StratButton()
+	{
+		SceneManager.LoadScene("Menu");//Menuシーンに移動
+	}
+}
